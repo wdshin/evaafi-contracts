15422e427da775c5113854a510528904b50506cf
Igor Konyakhin <nns2009@yandex.ru>
2023-03-17 21:58:48 +0100

    Synced op-codes between TypeScript and FunC

--- test/OpCodes.ts
+++ test/OpCodes.ts
@@ -1,10 +1,21 @@
 export const op = { // todo
 	transfer_notification: 0x7362d09c,
+	excesses: 0xd53276db,
+
 	init_master: 1,
 	init_user: 2,
+
 	update_price: 3,
 	update_config: 4,
-	supply: 5,
-	withdrawal: 6,
+
+	supply: 50,
+	supply_success: 51,
+
+	withdraw_master: 60,
+	withdraw_user: 61,
+	withdraw_collateralized: 62,
+	withdraw_success: 63,
+	withdraw_fail: 64,
+	
 	liquidate: 7,
 };
